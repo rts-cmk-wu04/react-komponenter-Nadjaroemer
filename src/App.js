09889368433217203918d5e9422f784336515cac
Nadjaroemer <nadja.kuti@gmail.com>
2021-01-13 21:45:02 +0100

    added architecture component js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Architecture from "./components/Architecture";
 import Services from "./components/Services";
 
 function App() {
   return (
     <>
+      <Architecture />
       <Services />
     </>
   );
